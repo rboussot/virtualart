10667c6a6bb5a86634493eeab82fb62cc4a4d90f
Romain Boussot <rboussot@gmail.com>
2017-08-01 18:03:35 +0200

    lightboxes sur les réalisations

--- app/controllers/realisations_controller.rb
+++ app/controllers/realisations_controller.rb
@@ -4,6 +4,25 @@ class RealisationsController < ApplicationController
   def index
     @realisations = Realisation.all
     @courses = Course.where(visible: true)
+    @propos = Block.find_by(tag: "propos")
+  end
+
+  def show
+    @realisations = Realisation.all
+    @courses = Course.where(visible: true)
+    @propos = Block.find_by(tag: "propos")
+    @realisation = Realisation.find(params[:id])
+    if @realisation.link.include? "http"
+      respond_to do |format|
+        format.html { render 'show', layout: false}
+        format.js { render 'picture' }
+      end
+    else
+      respond_to do |format|
+        format.html { render 'show', layout: false}
+        format.js { render 'video' }
+      end
+    end
   end
 
 end
